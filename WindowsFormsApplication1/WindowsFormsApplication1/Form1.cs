925964be6f648ee18363f8b4ba2c3328ce57fdb9
meysam bagheri <bagheri1210@gmail.com>
2017-07-21 20:54:13 +0430

    پروژه چك چاپ

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -8,6 +8,9 @@ namespace WindowsFormsApplication1
         public Form1()
         {
             InitializeComponent();
+
+
+
         }
 
         private void button1_Click(object sender, EventArgs e)
